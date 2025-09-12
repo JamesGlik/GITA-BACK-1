59cd8e61792f99e80070d98e45043242b3de141a
JamesGlik <sadoiani9900@gmail.com>
2025-09-13 00:33:11 +0400

    Task1-LukaSadoiani

---
+++ Task1-LukaSadoiani/main.js
@@ -0,0 +1,126 @@
+//1
+
+// function celsiusToFarenheit(celsius){
+//     farenheit = (celsius * 9/5) + 32
+//     console.log(farenheit)
+// }
+
+// celsiusToFarenheit(30)
+
+
+//2
+
+// function invertedWord(str){
+//     let inverted = 0;
+
+//     for(let i = str.length - 1; i >= 0; i--){
+//         inverted += str[i];
+//         console.log(inverted)
+//     }
+// }
+
+// invertedWord("pool")
+
+
+//3
+
+// function howManyWords(sentence){
+//     const words = sentence.split(' ')
+//     console.log(words.length)
+// }
+
+// howManyWords("This is for test")
+
+
+//4
+
+// function vowels(word){
+//     let count = 0;
+
+//     for(let i = 0; i < word.length; i++){
+//         if(word[i].includes("aeiouAEIOU")){
+//             count += 1
+//         }else{
+//             "Error"
+//         }
+//     }
+//     console.log(count)
+// }
+
+// vowels("aeiouAA")
+
+
+//5
+
+// function factorial(number){
+//     let multiplied = 1;
+
+//     for(let i = 1; i <= number; i++){
+//         multiplied *= i
+//     }
+//     console.log(multiplied)
+// }
+
+// factorial(5)
+
+
+//6
+
+// function evenNumberSum(number){
+//     let sum = 0;
+
+//     for(let i = 0; i <= number; i++){
+//         if(i % 2 == 0){
+//             sum += i
+//         }
+//     }
+//     console.log(sum)
+// }
+
+// evenNumberSum(10)
+
+
+//7
+
+// function isPassValid(password){
+//     if(password.length >= 8 && password[0] == password[0].toUpperCase()){
+//         console.log("Password is valid")
+//     }else{
+//         console.log("invalid password")
+//     }
+// }
+
+// isPassValid("Georgia5")
+
+
+//8
+
+// function studentGrade(grade){
+//     if(grade > 100 && grade < 0){
+//         console.log("Invalid Grade")
+//     }else if(grade >= 90){
+//         console.log("A")
+//     }else if(grade >= 80){
+//         console.log("B")
+//     }else if(grade >= 70){
+//         console.log("C")
+//     }else if(grade >= 50){
+//         console.log("D")
+//     }else if(grade >= 40){
+//         console.log("E")
+//     }else if(grade >= 20){
+//         console.log("F")
+//     }
+// }
+
+// studentGrade(71);
+
+
+
+
+
+
+
+
+
+
