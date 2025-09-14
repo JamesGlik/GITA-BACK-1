2a8e4c1257180840fc2689ce384d3d032eb03f56
JamesGlik <sadoiani9900@gmail.com>
2025-09-14 22:40:14 +0400

    Finished Task 2

---
+++ Task2-LukaSadoiani/main.js
@@ -0,0 +1,69 @@
+// function getAbbr(str){
+//     let answer = ""
+
+//     const words = str.split(' ')
+
+//     for (let word of words) {
+//         if(word){
+//             answer += word[0].toUpperCase() + ".";
+//         }
+//     }
+//     console.log(answer)
+// }
+
+// getAbbr("Luka Sadoiani")
+
+// function getSumOfDigit(number){
+//     let sum = 0;
+
+//     const digits = number.toString().split('')
+
+//     for(let digit of digits){
+//         if(digit){
+//             sum += parseInt(digit)   ////parseint da tostring() gagebashi gpt damexmara
+//         }
+//     }
+//     console.log(sum)
+// }
+
+// getSumOfDigit(1234)
+
+
+// function removeDuplicates(str) {
+//     let result = "";
+
+//     for (let i = 0; i < str.length; i++) {
+//         let currentChar = str[i];
+
+//         if (!result.includes(currentChar)) {         ////am lineshi gpt damexmara
+//             result += currentChar;
+//         }
+//     }
+
+//     console.log(result);
+// }
+
+// removeDuplicates("Banana")
+
+
+// function removeSpaces(str) {
+//     let result = "";
+
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] !== ' ') {
+//             result += str[i];
+//         }
+//     }
+
+//     console.log(result);
+// }
+
+
+// function reverseEachWord(str){
+//     let words = str.split(' ')
+//     let result = ""
+
+//     for(let i = 0; i < words.length; i++){
+        
+//     }
+// }
